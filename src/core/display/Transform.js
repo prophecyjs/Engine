0ac855b5724c604aaabe994eea9b0e899c8628e3
Johnny Mast <mastjohnny@gmail.com>
2017-12-07 23:38:02 +0100

    fixed scale bug because it wasnt playing with the rotate function.

--- src/core/display/Transform.js
+++ src/core/display/Transform.js
@@ -1,10 +1,20 @@
 export default class Transform {
     constructor() {
         this._rotation = 0;
-        this.scale = {
-            x:0, y:0
+        this._scale = {
+            x:0, y:0, factor: 0,
         }
+    }
 
+    set scale(scale) {
+        var x = (1 * scale);
+        this._scale.x =  x;
+        this._scale.y =  x;
+        this.scale.factor = x;
+    }
+
+    get scale() {
+        return this._scale;
     }
 
     set rotation(deg) {
--- src/core/sprite/canvas/canvasspriterenderer.js
+++ src/core/sprite/canvas/canvasspriterenderer.js
@@ -7,19 +7,21 @@ class CanvasSpriterRenderer {
 
         this.renderer.context.save();
 
-
-        if (sprite.transform.scale.x || sprite.transform.scale.y) {
+        if (sprite.transform.scale.factor > 0) {
             this.renderer.context.scale(sprite.transform.scale.x, sprite.transform.scale.y);
-
         }
 
-        if (sprite.transform.rotation)
-        {
-            this.renderer.context.translate(sprite.x + 0.5 * sprite.width, sprite.x + 0.5 * sprite.height);
-            this.renderer.context.rotate((Math.PI/180) * sprite.transform.rotation);
+
+        // TODO OOP the hell out of this below
+        if (sprite.transform.rotation) {
+            if (sprite.transform.scale.factor > 0) {
+                this.renderer.context.translate((sprite.x + 0.5 * sprite.width) / sprite.transform.scale.factor, (sprite.x + 0.5 * sprite.height) / sprite.transform.scale.factor);
+            } else {
+                this.renderer.context.translate((sprite.x + 0.5 * sprite.width), (sprite.x + 0.5 * sprite.height));
+            }
+            this.renderer.context.rotate((Math.PI / 180) * sprite.transform.rotation);
             this.renderer.context.translate(-(sprite.x + 0.5 * sprite.width), -(sprite.x + 0.5 * sprite.height));
         }
-
 
 
         this.renderer.context.drawImage(
@@ -31,8 +33,8 @@ class CanvasSpriterRenderer {
         );
 
 
-
-       this.renderer.context.restore();
+        this.renderer.context.restore();
     }
 }
+
 module.exports = CanvasSpriterRenderer;
